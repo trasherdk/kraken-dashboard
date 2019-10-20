1ddb96b4a37002acffb7b35aad1d8fb9cf5bf960
trasherdk <trasherdk@fumlersoft.dk>
2019-10-20 17:22:58 +0700

    Cleanin unused stuff

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiKey: "",
-  apiSecret: ""
+  apiKey: "AGwP7Eh2iKNIEOIz1lZvuPk0l57TP1oG75d2SduzOJvZXHlwsjM2YebR",
+  apiSecret: "WahM32jGwK3QHI3jUSFuLY5EqZ9rR7K4c70Jzfc5HbL4ZOZ+jdK4kUguPisx7Y4loxJZY1AwWcSMPmGBJ/WSeQ=="
 };
---
+++ test.js
@@ -0,0 +1,25 @@
+const KrakenClient = require('kraken-api');
+const keys         = require('./config/keys.js');
+const kraken       = new KrakenClient(keys.apiKey, keys.apiSecret);
+
+
+(async () => {
+    // Display user's balance
+    kraken.api('Balance')
+	.then(balance => {
+	    console.log('My Balance: %s', JSON.stringify(balance));
+	})
+    .catch(err => {
+    	console.log('Balance Error Catch: ', err)
+    })
+ 
+    // Get Ticker Info
+    //console.log(await kraken.api('Ticker', { pair : 'XXBTZUSD' }));
+    kraken.api('Ticker', { pair : 'XXMRZEUR' })
+    .then(data => {
+		console.log('Ticker Data: ', JSON.stringify(data))
+    })
+    .catch(err => {
+    	console.log('Ticker Error Catch: ', err)
+    })
+})();
