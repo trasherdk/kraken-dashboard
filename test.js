8a2d97cb6d70bd15542c9fb9349d4d256afbf3b3
trasherdk <trasherdk@fumlersoft.dk>
2019-10-21 11:56:57 +0700

    Housekeeping.

--- test.js
+++ test.js
@@ -10,16 +10,16 @@ const kraken       = new KrakenClient(keys.apiKey, keys.apiSecret);
 	    console.log('My Balance: %s', JSON.stringify(balance));
 	})
     .catch(err => {
-    	console.log('Balance Error Catch: ', err)
-    })
+    	console.log('Balance Error Catch: ', err);
+    });
  
     // Get Ticker Info
     //console.log(await kraken.api('Ticker', { pair : 'XXBTZUSD' }));
     kraken.api('Ticker', { pair : 'XXMRZEUR' })
     .then(data => {
-		console.log('Ticker Data: ', JSON.stringify(data))
+		console.log('Ticker Data: ', JSON.stringify(data));
     })
     .catch(err => {
-    	console.log('Ticker Error Catch: ', err)
-    })
+    	console.log('Ticker Error Catch: ', err);
+    });
 })();
